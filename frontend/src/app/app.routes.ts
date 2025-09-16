cd9e4ad6b8eddd232f5caab74842e9e09a0e28a1
Leevi Kesänen <AD1229@student.jamk.fi>
2025-09-16 13:11:26 +0300

    componentteja ja alustava routing pystyyn

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardPageComponent } from './features/dashboard-page/dashboard-page.component';
+import { LoginPageComponent } from './features/login-page/login-page.component';
+import { SigninPageComponent } from './features/signin-page/signin-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signin', component: SigninPageComponent },
+];
