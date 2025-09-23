0c1cb4476888824f6259f95fd399d01788c8f6ff
Leevi Kesänen <AD1229@student.jamk.fi>
2025-09-23 11:29:08 +0300

    dashboardia

--- frontend/src/app/features/dashboard-page/dashboard-page.component.ts
+++ frontend/src/app/features/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SocialBarComponent } from './social-bar/social-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [SocialBarComponent],
+  imports: [SocialBarComponent, DashboardComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css',
 })
