055c4990e54fccf05e9c130852d6015ad7c002e8
Leevi Kesänen <AD1229@student.jamk.fi>
2025-09-24 10:59:53 +0300

    dashboarida ja routausta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SigninPageComponent } from './features/signin-page/signin-page.componen
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { SocialBarComponent } from './features/dashboard-page/social-bar/social-bar.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,12 @@ export const routes: Routes = [
         path: 'dashboard',
         canActivate: [AuthGuard],
         component: DashboardPageComponent,
+        pathMatch: 'full',
+      },
+      {
+        path: 'social',
+        canActivate: [AuthGuard],
+        component: SocialBarComponent,
         pathMatch: 'full',
       },
     ],
