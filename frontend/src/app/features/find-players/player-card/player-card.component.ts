eb4d088675cec08bc31d218e627bdba4fb38cc1c
Tommi Hallila <151609696+thompson86@users.noreply.github.com>
2025-10-01 14:36:43 +0300

    Navigaatiopalkista tehty pienempi versio tarvittaessa. Lisätty pari uutta svg kuvaa. Globaaleja muuttujia siistitty. Luotu pelaajakortti. Tehty uusi komponentti tulevaisuuteen profiilisivulle.

--- frontend/src/app/features/find-players/player-card/player-card.component.ts
+++ frontend/src/app/features/find-players/player-card/player-card.component.ts
@@ -1,13 +1,30 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { User } from '../../../core/interfaces/user.model';
 
 @Component({
   selector: 'app-player-card',
-  imports: [],
   templateUrl: './player-card.component.html',
-  styleUrl: './player-card.component.css',
+  styleUrls: ['./player-card.component.css'],
 })
 export class PlayerCardComponent {
   @Input() user!: User;
+
+  isFavorite: boolean = false;
+
+  toggleFavorite(event: Event): void {
+    event.stopPropagation();
+    this.isFavorite = !this.isFavorite;
+  }
+
+  onInvite(): void {
+    console.log(`Send message to ${this.user.Username}`);
+  }
+
+  onProfile(): void {
+    console.log(`Opening profile of ${this.user.Username}`);
+  }
+
+  onAddFriend(): void {
+    console.log(`Friend request sent to ${this.user.Username}`);
+  }
 }
