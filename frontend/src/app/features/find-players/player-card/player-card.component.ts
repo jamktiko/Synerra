ace55ca57c36e76520b57647273d4577d5f93cc6
Lauri Halinen <ad0318@student.jamk.fi>
2025-10-28 12:47:41 +0200

    liput player cardeihin

--- frontend/src/app/features/find-players/player-card/player-card.component.ts
+++ frontend/src/app/features/find-players/player-card/player-card.component.ts
@@ -14,12 +14,40 @@ import { Router } from '@angular/router';
 })
 export class PlayerCardComponent {
   @Input() user!: User;
+  availableLanguages = [
+    { value: 'en', flag: 'https://flagcdn.com/gb.svg' },
+    { value: 'es', flag: 'https://flagcdn.com/es.svg' },
+    { value: 'fr', flag: 'https://flagcdn.com/fr.svg' },
+    { value: 'de', flag: 'https://flagcdn.com/de.svg' },
+    { value: 'zh', flag: 'https://flagcdn.com/cn.svg' },
+    { value: 'hi', flag: 'https://flagcdn.com/in.svg' },
+    { value: 'ar', flag: 'https://flagcdn.com/sa.svg' },
+    { value: 'pt', flag: 'https://flagcdn.com/pt.svg' },
+    { value: 'ru', flag: 'https://flagcdn.com/ru.svg' },
+    { value: 'ja', flag: 'https://flagcdn.com/jp.svg' },
+    { value: 'fi', flag: 'https://flagcdn.com/fi.svg' },
+    { value: 'sv', flag: 'https://flagcdn.com/se.svg' },
+  ];
 
   constructor(
     private friendService: FriendService,
     private chatService: ChatService,
     private router: Router
   ) {}
+
+  dropdownOpen = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+  get userLanguages(): string[] {
+    return this.user?.Languages ?? [];
+  }
+
+  getFlag(code: string): string {
+    const lang = this.availableLanguages.find((l) => l.value === code);
+    return lang ? lang.flag : '';
+  }
   onProfile(): void {
     console.log(`Opening profile of ${this.user.Username}`);
     this.router.navigate(['/dashboard/profile']);
