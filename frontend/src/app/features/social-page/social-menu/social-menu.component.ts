1cbace64c1965624a2eb9a6eb6d5559e8d65ae82
Leevi Kesänen <AD1229@student.jamk.fi>
2025-11-05 14:08:31 +0200

    social-page uusiksi. tulee sisältämään viestien lisäksi myös ilmoitukset

--- frontend/src/app/features/social-page/social-menu/social-menu.component.ts
+++ frontend/src/app/features/social-page/social-menu/social-menu.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, input, OnInit, output } from '@angular/core';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
-import { UserService } from '../../../core/services/user.service';
-import { UserStore } from '../../../core/stores/user.store';
 
 interface User {
   name: string;
@@ -11,44 +9,17 @@ interface User {
 }
 
 @Component({
+  standalone: true,
   selector: 'app-social-menu',
   imports: [CommonModule, ButtonComponent],
   templateUrl: './social-menu.component.html',
   styleUrl: './social-menu.component.css',
 })
-export class SocialMenuComponent implements OnInit {
-  activeTab: 'users' | 'groups' = 'users';
+export class SocialMenuComponent {
+  messagesTabShowing = input<boolean>();
+  notificationsClicked = output<string>();
 
-  directChats: any[] = [];
-  groupChats: any[] = [];
-
-  constructor(
-    private userService: UserService,
-    private userStore: UserStore,
-  ) {}
-
-  ngOnInit(): void {
-    const loggedInUser = this.userStore.getUser();
-    if (loggedInUser?.UserId) {
-      this.userService.getUserRooms(loggedInUser.UserId).subscribe({
-        next: (res) => {
-          const allChatRooms = res.rooms;
-          console.log(allChatRooms);
-          for (let i of allChatRooms) {
-            if (i.Members.length === 2) {
-              console.log('pushiings', i);
-              this.directChats.push(i);
-            } else if (i.Members.length > 2) {
-              this.groupChats.push(i);
-            }
-          }
-        },
-        error: (err) => {
-          console.error('Failed to fetch rooms:', err);
-        },
-      });
-    } else {
-      console.error('No userId found');
-    }
+  switchTab(tab: string) {
+    this.notificationsClicked.emit(tab);
   }
 }
