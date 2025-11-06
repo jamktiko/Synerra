24391de3284a7824e015e631ce7fa970f5296cef
Lauri Halinen <ad0318@student.jamk.fi>
2025-11-06 11:35:40 +0200

    notifikaatiot kaveripyynnöistä social pagelle

--- frontend/src/app/features/social-page/social-page.component.ts
+++ frontend/src/app/features/social-page/social-page.component.ts
@@ -7,6 +7,11 @@ import { ChatService } from '../../core/services/chat.service';
 import { NotificationsTabComponent } from './notifications-tab/notifications-tab.component';
 import { MessagesTabComponent } from './messages-tab/messages-tab.component';
 import { CommonModule } from '@angular/common';
+import { NotificationService } from '../../core/services/notification.service';
+import { FriendService } from '../../core/services/friend.service';
+import { UnreadMessage } from '../../core/interfaces/chatMessage';
+import { FriendRequest } from '../../core/interfaces/friendrequest.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-social-page',
@@ -25,11 +30,17 @@ export class SocialPageComponent implements OnInit {
   directChats: any[] = [];
   groupChats: any[] = [];
   noChats: boolean = false;
+  unreads: UnreadMessage[] = [];
+  pendingRequests: FriendRequest[] = [];
+  notifications: any[] = [];
+  private sub: Subscription | null = null;
 
   constructor(
     private userService: UserService,
     private userStore: UserStore,
     private chatService: ChatService,
+    private notificationService: NotificationService,
+    private friendService: FriendService
   ) {}
 
   ngOnInit(): void {
@@ -43,7 +54,7 @@ export class SocialPageComponent implements OnInit {
           for (let room of allChatRooms) {
             // remove logged-in user from members
             room.Members = room.Members.filter(
-              (m: any) => m.UserId !== loggedInUser.UserId,
+              (m: any) => m.UserId !== loggedInUser.UserId
             );
 
             if (room.Members.length === 1) {
@@ -62,6 +73,28 @@ export class SocialPageComponent implements OnInit {
     } else {
       console.error('No userId found');
     }
+    // Listens to friend requests
+    this.friendService.pendingRequests$.subscribe({
+      next: (requests) => {
+        this.pendingRequests = requests;
+        console.log(
+          'Pending friend requests ON SOCIAL PAGE:',
+          this.pendingRequests
+        );
+      },
+      error: (err) =>
+        console.error('Failed to load pending requests ON SOCIAL PAGE', err),
+    });
+
+    // Fetch initial data
+    this.friendService.getPendingRequests().subscribe();
+
+    // Subscribe to incoming notifications
+    this.sub = this.notificationService.notifications$.subscribe((data) => {
+      console.log('Received notification in SOCIAL:', data);
+      this.notifications.push(data);
+      console.log(this.notifications);
+    });
   }
 
   tabSwitch(tab: string) {
