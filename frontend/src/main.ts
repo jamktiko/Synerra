f137b5a5c8c1c20457b22f5c98fd9dac9a8942d1
Leevi Kesänen <AD1229@student.jamk.fi>
2025-11-19 19:27:11 +0200

    console.logit disabloitu production-modessa

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,16 @@
+import { enableProdMode, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+// Checks if the app is not being run in development mode (so if it's production)
+// and disables all console.logs if so
+if (!isDevMode()) {
+  enableProdMode();
+
+  // Disable console.log in production
+  window.console.log = () => {};
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
