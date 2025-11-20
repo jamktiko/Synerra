3c301a6634650192571695186165a1ae691986b8
Leevi Kesänen <AD1229@student.jamk.fi>
2025-11-20 10:15:43 +0200

    kaverit näkyy social-pagella

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,12 +3,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// Checks if the app is not being run in development mode (so if it's production)
+// Checks if the app is not being run in development mode (so if it's in production)
 // and disables all console.logs if so
 if (!isDevMode()) {
   enableProdMode();
 
-  // Disable console.log in production
+  // Redirects the logs to nowhere
   window.console.log = () => {};
 }
 
